c36d8e8b645ab46d2249f3b218a4b9e229523b72
RollW <73376341+roll-w@users.noreply.github.com>
2025-06-14 14:55:37 +0800

    feat: add implementations for HTTP status code handling.

---
+++ web-common-core/src/main/java/tech/rollw/common/web/MatchBestStatusCodeProvider.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2023-2025 RollW
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tech.rollw.common.web;
+
+import org.springframework.http.HttpMethod;
+
+/**
+ * A status code provider that converts a given status to the best matching HTTP status code
+ * based on the HTTP method used.
+ * <p>
+ * Only converts the status code if it is 200, otherwise returns the original status code.
+ *
+ * @author RollW
+ */
+public class MatchBestStatusCodeProvider implements StatusCodeProvider {
+    @Override
+    public int getStatusCode(Status status, HttpMethod httpMethod) {
+        int code = status.getErrorCode().getStatus();
+        if (code != 200) {
+            return code;
+        }
+        if (httpMethod.equals(HttpMethod.POST) || httpMethod.equals(HttpMethod.PUT) ||
+                httpMethod.equals(HttpMethod.PATCH)) {
+            return 201;
+        }
+        if (httpMethod.equals(HttpMethod.DELETE)) {
+            return 204;
+        }
+        return 200;
+    }
+}
